4c100963446946c91586d1760530611a59e5e77b
starrtree <max@starrtree.org>
2026-05-04 18:51:15 -0400

    Align frame pipeline with existing MOV source video

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export function useMediaQuery(query) {
+  const [matches, setMatches] = useState(() => window.matchMedia(query).matches);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    const onChange = () => setMatches(media.matches);
+    media.addEventListener('change', onChange);
+    return () => media.removeEventListener('change', onChange);
+  }, [query]);
+
+  return matches;
+}
